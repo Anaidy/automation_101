58850daf025bd377e5760696e57c4b29de9a18d7
CarlosRG <cramirez.griego@gmail.com>
2020-08-29 11:04:02 -0700

    Lesson006 - WebPage Static Class

--- automation_101/automation_101/Program.cs
+++ automation_101/automation_101/Program.cs
@@ -26,57 +26,81 @@ namespace automation_101
              * ***/
 
             /***
-             * Lesson 006 - Set implicit wait and timeout at the very beginning
+             * Lesson 007 - Create a Static WebPage class 
              * 
-             * We move such settings to Step 1, right after we create the Driver
+             * Since the WebDriver is something that is shared among any page we navigate
+             * we are going to create a class specific for it, and the actions we can perform
              * 
+             * Possible drawbacks of this approach: the driver will be in a static variable,
+             * this means that if we want another WebPage in another driver object we'll need to use tricks
+             * But this approach works really good if you only need one browser open at the time
+             * 
+             * We will create the WebPage.cs class next to this file.
+             * This class will have "friendly" static methods for:
+             * - Openning Chrome
+             * - Navigate to a URL
+             * - Maximize Browser
+             * - Close Browser
+             * - Set Implicit Wait and Timeout
+             * 
+             * From anywhere, we will be able to interact with the driver using WebPage.TestWebDriver
+             * 
+             * Compare Lesson005 vs Lesson006 code ;)
              * 
              * ***/
 
 
-            IWebDriver WDObject = new ChromeDriver();  // We open a Chrome Web Browser
-            WDObject.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
-            WDObject.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            WebPage.OpenChromeBrowser();
+            WebPage.SetPageTimeOutSeconds(15);
+            WebPage.SetImplicitWait(5);
+            WebPage.MaximizeBrowser();
+
 
             string url = "https://pokemondb.net/";
-            WDObject.Manage().Window.Maximize();
+            WebPage.NavigateToThisPage(url);
 
-            WDObject.Navigate().GoToUrl(url);
 
-            IWebElement NationalPokedexQuickLink = WDObject.FindElement(By.CssSelector("main[id='main'] a[href='/pokedex/national']"));
+            IWebElement NationalPokedexQuickLink = WebPage.TestWebDriver.FindElement(By.CssSelector("main[id='main'] a[href='/pokedex/national']"));
             NationalPokedexQuickLink.Click();
 
-            IWebElement Gen8Link = WDObject.FindElement(By.CssSelector("a[href='#gen-8']"));
+
+            IWebElement Gen8Link = WebPage.TestWebDriver.FindElement(By.CssSelector("a[href='#gen-8']"));
             Gen8Link.Click();
+
 
             string SelectPokemon = "dreepy"; //We manually select a Pokemon to click
 
+
             string Selector = "a[href='/pokedex/"+ SelectPokemon + "']";  //We have to manually forge HOW we are going to find this pokemon
-            IWebElement PokemonTile = WDObject.FindElement(By.CssSelector(Selector));  //Here we find the Pokemon element
-            Actions actions = new Actions(WDObject);  //Since this Pokemon is at the bottom of the page we WANT to scroll there first
+            IWebElement PokemonTile = WebPage.TestWebDriver.FindElement(By.CssSelector(Selector));  //Here we find the Pokemon element
+            Actions actions = new Actions(WebPage.TestWebDriver);  //Since this Pokemon is at the bottom of the page we WANT to scroll there first
             actions.MoveToElement(PokemonTile);  //Here we move to the Pokemon tile
             PokemonTile.Click();
 
-            IWebElement PokemonNameHeader = WDObject.FindElement(By.CssSelector("main>h1"));
+
+            IWebElement PokemonNameHeader = WebPage.TestWebDriver.FindElement(By.CssSelector("main>h1"));
             Console.WriteLine(PokemonNameHeader.Text);  //The element name is printed in console 
 
+
             //Step 8 tweak
-            IWebElement PokemonInfoContainer = WDObject.FindElement(By.CssSelector("div[class='tabs-panel active'][id^='tab-basic-']"));
+            IWebElement PokemonInfoContainer = WebPage.TestWebDriver.FindElement(By.CssSelector("div[class='tabs-panel active'][id^='tab-basic-']"));
             IWebElement PokemonInfoContainer_BasicInfo = PokemonInfoContainer.FindElement(By.CssSelector("div:nth-child(1)>div[class$='text-center']+div:nth-child(2)>h2+table.vitals-table"));
             IWebElement PokemonNationalDexNumber = PokemonInfoContainer_BasicInfo.FindElement(By.CssSelector("tbody>tr:nth-child(1) >td"));
             Console.WriteLine(PokemonNationalDexNumber.Text);  //The element name is printed in console 
+
 
             // Step 9
             IWebElement PokemonInfoContainer_StatsContainer = PokemonInfoContainer.FindElement(By.CssSelector("div:nth-child(2)>div:nth-child(1)"));
             IWebElement PokemonInfoContainer_StatsContainer_BaseStatHP = PokemonInfoContainer_StatsContainer.FindElement(By.CssSelector("table.vitals-table tbody>tr:nth-child(1) td:nth-of-type(1)"));
             Console.WriteLine(PokemonInfoContainer_StatsContainer_BaseStatHP.Text);  //The element name is printed in console 
 
+
             //Step 10
-            IWebElement TacosElement = WDObject.FindElement(By.CssSelector("tacomon"));  
+            IWebElement TacosElement = WebPage.TestWebDriver.FindElement(By.CssSelector("tacomon"));
+
 
             //Step 11
-            Thread.Sleep(3000);
-            WDObject.Quit(); // We end the execution
+            WebPage.CloseBrowser();
         }
     }
 }
---
+++ automation_101/automation_101/WebPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace automation_101
+{
+    public class WebPage
+    {
+        public static IWebDriver TestWebDriver;
+
+        public static void OpenChromeBrowser()
+        {
+            TestWebDriver = new ChromeDriver();
+        }
+
+
+        public static void NavigateToThisPage(string url)
+        {
+            TestWebDriver.Navigate().GoToUrl(url);
+        }
+
+
+        public static void MaximizeBrowser()
+        {
+            TestWebDriver.Manage().Window.Maximize();
+        }
+
+        public static void CloseBrowser()
+        {
+            TestWebDriver.Quit();
+        }
+
+        public static void SetPageTimeOutSeconds(int i)
+        {
+            TestWebDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(i);
+        }
+
+        public static void SetImplicitWait(int i)
+        {
+            TestWebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(i);
+        }
+
+    }
+}
